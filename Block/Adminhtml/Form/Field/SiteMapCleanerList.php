338702c5b4e0d9fd9d232d06635a3dd22099c7e0
anass Touati <anass.touati@hagergroup-external.com>
2023-03-28 21:32:26 +0200

    move import export CSV files logic to Base Module

--- Block/Adminhtml/Form/Field/SiteMapCleanerList.php
+++ Block/Adminhtml/Form/Field/SiteMapCleanerList.php
@@ -10,40 +10,16 @@ declare(strict_types=1);
 
 namespace AnassTouatiCoder\SitemapRemoveItem\Block\Adminhtml\Form\Field;
 
-use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+use AnassTouatiCoder\Base\Block\Adminhtml\Form\FieldArray;
 use Magento\Framework\DataObject;
 use Magento\Framework\Exception\LocalizedException;
 
-class SiteMapCleanerList extends AbstractFieldArray
+class SiteMapCleanerList extends FieldArray
 {
-    /*
-     * Additional buttons
+    /**
+     * @var string
      */
-    private const ADDITIONAL_BUTTON_LIST = [
-        'sitemap_remove_item_import_button' => [
-            'label' => 'Import',
-            'class' => 'action-default scalable',
-            'data_attribute' => [
-                'mage-init' => [
-                    'AnassTouatiCoder_SitemapRemoveItem/js/import-button' => [
-                        'url' => 'atouatiSitemap/index/import',
-                        'content' => '<p>All existing items in the list will be replaced with csv file content.</p>'
-                    ]
-                ]
-            ]
-        ],
-        'sitemap_remove_item_export_button' => [
-            'label' => 'Export',
-            'class' => 'action-default scalable secondary',
-            'data_attribute' => [
-                'mage-init' => [
-                    'AnassTouatiCoder_SitemapRemoveItem/js/export-button' => [
-                        'url' => 'atouatiSitemap/index/export'
-                    ]
-                ]
-            ]
-        ]
-    ];
+    protected $dataConfig = 'atouati_data_config_sitemapremoveitem';
 
     /**
      * @var URIType
@@ -100,53 +76,5 @@ class SiteMapCleanerList extends AbstractFieldArray
             'option_extra_attrs',
             $optionExtraAttr
         );
-    }
-
-    /**
-     * Prepare the layout
-     *
-     * @return $this
-     */
-    protected function _prepareLayout()
-    {
-        parent::_prepareLayout();
-        foreach (self::ADDITIONAL_BUTTON_LIST as $buttonAlias => $buttonData) {
-            $this->getGenerateComponentData($buttonData);
-            $this->setChild(
-                $buttonAlias,
-                $this->getLayout()->createBlock(\Magento\Backend\Block\Widget\Button::class)->setData($buttonData)
-            );
-        }
-
-        return $this;
-    }
-
-    /**
-     * Add import export buttons
-     * @return string
-     * @throws \Exception
-     */
-    protected function _toHtml()
-    {
-        return parent::_toHtml() . $this->getChildHtml();
-    }
-
-    /**
-     * Generate dynamic data for the component
-     *
-     * @param array $element
-     * @return void
-     */
-    private function getGenerateComponentDAta(array &$element): void
-    {
-        $jsComponentName = array_key_first($element['data_attribute']['mage-init']);
-        $element['data_attribute']['mage-init'][$jsComponentName]['url'] =
-            $this->getUrl($element['data_attribute']['mage-init'][$jsComponentName]['url']);
-
-        $element['data_attribute']['mage-init'][$jsComponentName]['webSiteId'] =
-            $this->getRequest()->getParam('website');
-        $element['data_attribute']['mage-init'][$jsComponentName]['storeId'] =
-            $this->getRequest()->getParam('store');
-        $element['data_attribute']['mage-init'][$jsComponentName]['formKey'] = $this->getFormKey();
     }
 }
